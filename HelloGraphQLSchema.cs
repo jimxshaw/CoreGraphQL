422b83e89731ac946e750a2a0be178723fffbada
Jim Shaw <jimxshaw@gmail.com>
2018-07-30 09:02:17 -0500

    Add singletons to Startup. Add schema class.

---
+++ HelloGraphQLSchema.cs
@@ -0,0 +1,17 @@
+﻿using GraphQL.Types;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projects
+{
+  public class HelloGraphQLSchema : Schema
+  {
+    public HelloGraphQLSchema(HelloGraphQLQuery query)
+    {
+      Query = query;
+    }
+  }
+}
--- Startup.cs
+++ Startup.cs
@@ -20,6 +20,9 @@ namespace projects
     // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
     public void ConfigureServices(IServiceCollection services)
     {
+      services.AddSingleton<IDocumentWriter, DocumentWriter>();
+      services.AddSingleton<IDocumentExecuter, DocumentExecuter>();
+      services.AddSingleton<HelloGraphQLQuery>();
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
